2d1a4a9c54b64b24be41214016d80227ff8877cc
Yan Toton <develroman320@gmail.com>
2025-08-08 19:50:12 +0600

    Create good night sir.py

---
+++ good night sir.py
@@ -0,0 +1,32 @@
+# list = [1,3,5, 89, 8753,"hi"]
+# listsize1 = len(list)
+# for i in range(0, listsize1):
+#     print(list[i])
+#
+# from datetime import datetime
+# now: datetime = datetime.now()
+# print(f'{now:%x}')
+# print(f'{now:%c}')
+# print(f'{now:%H:%M:%S}')
+
+# My own 2nd program
+
+# from datetime import datetime
+#
+# # Get the current datetime object
+# current_datetime = datetime.now()
+#
+# # Extract the hour as an integer
+# hour_integer = current_datetime.hour
+#
+# if 6 > hour_integer <12:
+#      print("Good Morning Sir!")
+# elif 12 < hour_integer > 20:
+#     print("Good Afternoon Sir!")
+# else:
+#     print(" Good Night Sir!")
+
+
+
+
+
