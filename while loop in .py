673bd813202eaa78c435a1475df80a1d5d28075e
Yan Toton <develroman320@gmail.com>
2025-08-10 03:10:52 +0600

    Create while loop in .py

---
+++ while loop in .py
@@ -0,0 +1,65 @@
+count = 10
+while(count > 0):
+  print(count)
+  count = count -1
+
+count = 10
+while(count > 0):
+  print(count)
+  count = count +1
+
+y = 0
+while (y < 10):
+  print(y)
+
+y = 0
+while (y < 10):
+  print(y)
+y = y +1
+
+t = int(input("Enter your number:))
+while (y < 36):
+   t = int(input("Enter your number:))
+   print(y)
+  y = y +1
+
+name = input("What is your name?")
+while name == "":
+    print("please enter your name")
+else:
+    print(f"Hello {name}")
+
+
+
+name = input("What is your name?")
+
+while name == "":
+    print("please enter your name")
+    name = input("What is your name?")
+print(f"Hello {name}")
+
+
+
+age = int(input("What is your age?"))
+
+while age < 0 :
+    print("age can't be negative")
+    age = int(input(" What is your age?"))
+print(f"your age is {age}")
+
+
+while not food == "q":
+    print(f"Your favorite food is {food}")
+    food = input("What is your another favorite food? (q to quit)")
+print("just bye")
+
+num = int(input("Enter a # between 1 - 10:"))
+
+
+while num < 1 or num > 10:
+    print(f"{num} is invalid")
+    num = int(input("Enter a # between 1 - 10:"))
+
+print(f"{num} is valid")
+
+
