9baef9fb2e4a69721ba1178ef1dcf386733f067c
Yan Toton <develroman320@gmail.com>
2025-08-17 05:52:49 +0600

    Create Add two numbers.py

---
+++ Add two numbers.py
@@ -0,0 +1,16 @@
+#Solution-1 (with int-user-input) 
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+print("The sum is:",num1+num2)
+
+#Solution-2(with float-user-input)
+num1 = float(input("Enter a number:"))
+num2 = float(input("Enter another number:"))
+sum = num1+num2
+print("The sum is:",sum)
+
+#Solution-3 (with-pre-defined-variables).
+num1 = 80
+num2 = 6
+sum = num1+num2
+print("The total sum of given two numbers is:", sum)
