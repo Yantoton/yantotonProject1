443a758737d200a5531f9aa1b8d78978b2ed0167
Yan Toton <develroman320@gmail.com>
2025-08-18 20:05:39 +0600

    Create List program in.py

---
+++ List program in.py
@@ -0,0 +1,4 @@
+farm_list = ["Milking cow -100","Fattening Bull-200","Calf -100","Dog -40","Goat -20","Parot Birds -500","Sheep -25","fish_1000"]
+for farm in farm_list:
+    print(farm)
+print(farm_list)
