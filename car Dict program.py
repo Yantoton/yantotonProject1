b1ad6c01dd78575071f551eb073059d0449dd51e
Yan Toton <develroman320@gmail.com>
2025-08-19 00:22:45 +0600

    Create car Dict program.py

---
+++ car Dict program.py
@@ -0,0 +1,12 @@
+car = {
+    "brand": "toyota",
+    "model": "gt86",
+    "year": 2012,
+}
+car["color"] = "Midnight Blue"
+g = car.keys()
+t = car.values()
+o = car["color"]
+print(g)
+print(t)
+print(o)
