cdfc8522ca8b1c0322257ac2fbd2a5c1d040571e
Yan Toton <develroman320@gmail.com>
2025-08-18 22:11:32 +0600

    Create thisdict program.py

---
+++ thisdict program.py
@@ -0,0 +1,25 @@
+thisdict = {
+   "brand": "Ford",
+    "model": "Mustang",
+    "year1": 1964,
+    "year2": 2020
+}
+print(thisdict["brand"])
+print(thisdict["model"])
+print(thisdict["year1"])
+print(thisdict["year2"])
+
+
+car = {
+   "brand": "Ford",
+   "model": "Mustang",
+    "year": 1964
+}
+
+car["color"] = "Black"
+m = car.keys()
+y = car.values()
+t = car["color"]
+print(m)
+print(y)
+print(t)
