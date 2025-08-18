1beab2943ea1130e9534b2c9180d051dc2ec53c5
Yan Toton <develroman320@gmail.com>
2025-08-18 20:07:54 +0600

    Create Function list program.py

---
+++ Function list program.py
@@ -0,0 +1,7 @@
+def farm_list():
+    farm_list = ['Milking cow-100','calf-100','Fattening bull-200','Dog-50','Birds-2000','Sheep-30','Goat-20','Fish-100']
+    for farm in farm_list:
+        print(farm)
+        print(type(farm_list))
+
+farm_list()
