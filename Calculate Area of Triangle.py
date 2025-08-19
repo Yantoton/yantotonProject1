7fda227213cb2b36c7741edad3f58d7a14abf35b
Yan Toton <develroman320@gmail.com>
2025-08-20 04:10:05 +0600

    Create Calculate Area of Triangle.py

---
+++ Calculate Area of Triangle.py
@@ -0,0 +1,6 @@
+base = float(input("Please enter the base of number: "))
+high = float(input("Please enter the high base of number: "))
+
+area = base * high
+
+print("The area of your number is " + str(area))
