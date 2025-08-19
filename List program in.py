744b07816efe223505fb1cd4d4ff6aa6ff267755
Yan Toton <develroman320@gmail.com>
2025-08-20 02:13:12 +0600

    Update List program in.py

--- List program in.py
+++ List program in.py
@@ -2,3 +2,14 @@ farm_list = ["Milking cow -100","Fattening Bull-200","Calf -100","Dog -40","Goat
 for farm in farm_list:
     print(farm)
 print(farm_list)
+
+t = [11,21,4,5,15,16,1,16,76,79,64]
+
+m = [86000,2456,786210]
+
+c = [12786,9746564,47465,468643]
+
+y = t+m+c
+print(y)
+for t,m,c in zip(t,m,c):
+    print(t,m,c)
