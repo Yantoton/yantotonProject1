c4cca2d0ea41799d92fb5b94d85145e3c1bea9a2
Yan Toton <develroman320@gmail.com>
2025-08-22 08:44:43 +0600

    Create What is Tuple program in.py

---
+++ What is Tuple program in.py
@@ -0,0 +1,46 @@
+this_tuple = ("Farm", "Car", "Code")
+print(this_tuple)
+
+#Items[0]:
+this_tuple = ("Farm", "Car", "Code")
+print(this_tuple[0])
+
+#Allow Duplicates:
+this_tuple = ("Farm", "Car", "Code", "Farm", "Car", "Code")
+print(this_tuple)
+
+#Length - len():
+this_tuple = ("Farm", "Car", "Code")
+print(len(this_tuple))
+
+#Create Tuple With One Item:
+my_tuple = ("Think Big",)
+print(type(my_tuple))
+#Not a Tuple
+my_tuple = ("Think Big")
+print(type(my_tuple))
+
+#Tuple Items - Data Types
+
+tuple1 = ("Farm" "Car" "Code")
+tuple2 = (246108)
+tuple3 = (True, False, True)
+
+print(tuple1)
+print(tuple2)
+print(tuple3)
+print(type(tuple1),tuple1)
+print(type(tuple2),tuple2)
+
+#The tuple() Constructor
+my_tuple1 = tuple (("Farm" ,"Car" ,"Code"))
+print(my_tuple1)
+
+
+
+
+
+
+
+
+
