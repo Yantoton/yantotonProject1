d0ba81a3b5da1aa03379f9dfadc22bb9281bb2d5
Yan Toton <develroman320@gmail.com>
2025-08-24 01:27:55 +0600

    Create For While Tuple program.py

---
+++ For While Tuple program.py
@@ -0,0 +1,7 @@
+my_tuple1 = tuple (("Farm" ,"Car" ,"Code"))
+for item in my_tuple1:
+    print(item)
+while True:
+    if tuple(my_tuple1) == ("Farm","Car","Code"):
+        print('function is working')
+print(my_tuple1)
