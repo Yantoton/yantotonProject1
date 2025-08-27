77a39f59f3e632976fac7046ab8f04502ba900e5
Yan Toton <develroman320@gmail.com>
2025-08-28 04:52:34 +0600

    Create format tuple in.py

---
+++ format tuple in.py
@@ -0,0 +1,5 @@
+my_tuple1 = tuple (("Farm" ,"Car" ,"Code"))
+print(my_tuple1)
+
+hil = "welcome to my {} house and this is my {} colection and i am also a {}r ".format(my_tuple1[0], my_tuple1[1], my_tuple1[2])
+print(hil)
