16df4efc827372900d7d639c3e312e122dec29cf
Yan Toton <develroman320@gmail.com>
2025-08-30 03:29:01 +0600

    Create My Bike & My Phone program  in.py

---
+++ Still Useing My Bike&My Phone program  in.py
@@ -0,0 +1,7 @@
+tem = 'apple'
+tx = "iphone 7"
+print(f"I am using {tem} brand phone!... & My phone model is {tx}!...")
+
+brand = 'Yamaha'
+model = 'R15 V2'
+print(f"why i am using {brand} brand bike & My bike model is {model} it's so fast!...")
