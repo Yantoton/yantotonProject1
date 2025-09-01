f36f73448883bfd9b1e1d04553825b3ec54d8275
Yan Toton <develroman320@gmail.com>
2025-09-02 05:01:11 +0600

    Update Fibonacci Sequence? def Fib.py

--- Fibonacci Sequence def Fib.py
+++ Fibonacci Sequence def Fib.py
@@ -4,3 +4,22 @@ def fib(n):
     else:
         return fib(n - 1) + fib(n - 2)
 print(fib(9))
+
+
+def fib(n):
+    a = 0
+    b = 1
+
+    if n == 1:
+        print(a)
+    else:
+        print(a)
+        print(b)
+
+        for i in range(2,n):
+           c = a + b
+           a = b
+           b = c
+           print(c)
+
+fib(100)
