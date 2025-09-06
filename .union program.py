532da452b11ba581503cf32efdb1038fce7d1a11
Yan Toton <develroman320@gmail.com>
2025-09-07 03:53:57 +0600

    Create .union program.py

---
+++ .union program.py
@@ -0,0 +1,15 @@
+#Using Third variable
+s1 = {1,4,6,8}
+s2 = {2,5,3,7}
+s3 = s1 | s2
+print(s3)
+
+#Usin Vertical line 
+s1 = {1,4,6,8}
+s2 = {2,5,3,7}
+print(s1 | s2)
+
+#Using union 
+s1 = {1,4,6,8}
+s2 = {2,5,3,7}
+print(s1.union(s2))
