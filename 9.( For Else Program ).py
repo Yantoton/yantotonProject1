bc2ffdfc9baf01fe77c2da00e7d04c9489271991
Yan Toton <develroman320@gmail.com>
2025-09-12 03:49:49 +0600

    Create 9.( For Else Program ).py

---
+++ 9.( For Else Program ).py
@@ -0,0 +1,8 @@
+names = ['yan','madina','toton','ruhu']
+
+for name in names:
+    if 'madina' == name:
+        print("baba love u madina.")
+        break
+else:
+        print("not here anymore")
