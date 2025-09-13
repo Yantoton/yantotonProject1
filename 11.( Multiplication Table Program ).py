46bfd4762133228da0d64272413c1240152ad3ad
Yan Toton <develroman320@gmail.com>
2025-09-14 04:18:10 +0600

    Create 11.( Multiplication Table Program ).py

---
+++ 11.( Multiplication Table Program ).py
@@ -0,0 +1,8 @@
+x = int(input("Enter a number:"))
+print(f"Multiplication of {x} is:")
+
+print("starting of program")
+for i in range(1,11):
+    print(f"{int(x)} x {i} = {int(x)*i}")
+
+print("end of program")
