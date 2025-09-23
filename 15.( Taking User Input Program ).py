8bb7a30d3d6bd78603b609a017916263c88ad186
Yan Toton <develroman320@gmail.com>
2025-09-24 04:47:48 +0600

    Create 15.( Taking User Input Program ).py

---
+++ 15.( Taking User Input Program ).py
@@ -0,0 +1,10 @@
+a = input("Enter your name:")
+print("My name is",a)
+
+s = input("Enter your age:")
+print("My age is",s)
+
+n = input("Enter your home town:")
+print("My home town is",n)
+
+print(a + s + n)
