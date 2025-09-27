1ec4cf1b97847aa9fcac4249857b8deb16c33c34
Yan Toton <develroman320@gmail.com>
2025-09-28 04:33:53 +0600

    Create 17.( Strings Slicing and Operations on Strings in Python ).py

---
+++ 17.( Strings Slicing and Operations on Strings in Python ).py
@@ -0,0 +1,12 @@
+pc = "Computer Science"
+len1 = len(pc)
+print("Computer Science is a",len1,"letter word!....")
+
+mobile = "Apple"
+mobile2 = len(mobile)
+print(mobile2)
+print(mobile[0:5])
+print(mobile[1:3])
+print(mobile[:5])
+print(mobile[0:len(mobile)-2])
+print(mobile[-4:len(mobile)-1])
