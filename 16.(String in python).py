13146d60d4bbfe81bcd42f897f6de6cda0630cc2
Yan Toton <develroman320@gmail.com>
2025-09-28 03:33:13 +0600

    Create 16.(String in python).py

---
+++ 16.(String in python).py
@@ -0,0 +1,25 @@
+friend1 = "Mahiyan"
+friend2 = "Yan"
+friend3 = "toton"
+apple = ('''_ say,'
+i have 50k so that's why
+"I want to buy i phone13''')
+
+phone =('''_say ,'yan
+ you can't buy i phone 13 in 50k       
+  if your budget 50k you can buy i phone 12 pro''')
+
+print("Hello " + friend1)
+print(friend2 + apple  )
+print(friend3 + phone)
+print(friend1[0])
+print(friend1[1])
+print(friend1[2])
+print(friend1[3])
+print(friend1[4])
+print(friend1[5])
+print(friend1[6])
+
+print("Using for loop in string\n")
+for frnd in friend1:
+    print(frnd)
