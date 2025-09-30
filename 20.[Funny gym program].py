1894ed71f32b5f198efe63d853ded92cf1dbd94b
Yan Toton <develroman320@gmail.com>
2025-10-01 04:28:19 +0600

    Create 20.[Funny gym program].py

---
+++ 20.[Funny gym program].py
@@ -0,0 +1,16 @@
+name = input("what is your name sir?:")
+print(f'Hello {name} sir welcome to fitnex gym\n My name is Fahim admin trainer of this gym ')
+
+qustion = ("So..!sir Do you want to join our gym? or visit our gym?..",'join','visit')
+print(qustion)
+
+answer = input("Tell me sir how can I help you?")
+
+if answer == "join":
+    print("Okay, sir, you can start exercising\n from today if you want.")
+
+elif answer == "visit":
+    print("Okay, sir, Anytime you can join our gym")
+
+else:
+    print("Haram zada bedigate kuttar bacha new market\n khan kir pola mojalos?....")
