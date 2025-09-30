aa7b6d42bbeddbdf549fd9425fc061ce7a7469eb
Yan Toton <develroman320@gmail.com>
2025-10-01 04:26:55 +0600

    Create 19.[ just for fun ].py

---
+++ 19.[ just for fun ].py
@@ -0,0 +1,14 @@
+name =input("What is your name sir?: ")
+print(f'welcome {name} in my shop')
+
+qustion =(" Do u want to buy anything or visit my shop?...",'yes','no')
+print(qustion)
+
+answer = input("tell me sir?:")
+
+if answer == "yes":
+    print("How can i help you sir???")
+elif answer == "no":
+    print("If like anything tell me sir!!! ")
+else:
+    print("khan kir pola!...! Mojalos???")
