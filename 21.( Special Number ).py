1d7326c9d6e2f5f86b166d9cf28f30ae70e40a87
Yan Toton <develroman320@gmail.com>
2025-10-02 01:41:00 +0600

    Create 21.( Special Number ).py

---
+++ 21.( Special Number ).py
@@ -0,0 +1,11 @@
+
+pzn_num = int(input("Enter the value number:"))
+
+if   pzn_num < 0:
+    print("number is Negative")
+elif pzn_num == 0:
+    print("number is zero")
+elif pzn_num == 86:
+    print("number is special.")
+else:
+    print("number is  positive")
