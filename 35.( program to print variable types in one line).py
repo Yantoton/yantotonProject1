0ce92e3abc4b13443421063dabc86fc18f863c68
Yan Toton <develroman320@gmail.com>
2025-11-16 22:36:00 +0600

    Add program to print variable types in one line

---
+++ 35.( program to print variable types in one line).py
@@ -0,0 +1,5 @@
+a = 123
+b = "yan"
+c = True
+d = None
+print(f"the type of \'{a}\' is {type(a)}\nthe type of \'{b}\' is {type(b)}\nthe type of \'{c}\' is {type(c)}\nthe type of \'{d}\' is {type(d)}")
