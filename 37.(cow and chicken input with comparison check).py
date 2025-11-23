17b4c2fe1de3b4d9b5e04542f522380e1e07ced9
Yan Toton <develroman320@gmail.com>
2025-11-23 23:05:16 +0600

    Add cow and chicken input comparison check

---
+++ 37.(cow and chicken input with comparison check).py
@@ -0,0 +1,3 @@
+cow =int(input("Enter the how many cow\'s in farm:"))#TAKEING USER COW INPUT
+chicken=int(input("Enter the how many chicken\'s in farm:"))#TAKEING USER CHICKEN INPUT
+print(f"so let\'s check:",cow==100 or chicken==100)#CHECKING THE CORRECT ANSWER COW & CHICKEN VALUE
