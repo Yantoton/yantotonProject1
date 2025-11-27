8744af2d4e8ee0ae3d8724d551a84031c2035b93
Yan Toton <develroman320@gmail.com>
2025-11-28 01:57:38 +0600

    Add integer conversion examples in Python
    This script demonstrates conversions of an integer to various formats including float, hexadecimal, octal, binary, and Unicode.

---
+++ 38.(conversions for integer to various formats).py
@@ -0,0 +1,20 @@
+a = 13  #THIS IS A integer VALUE
+print(a)# PRINTING THE VALUE OF INTEGER
+
+b = float(a) #THIS IS A floaT VALUE
+print(b)# PRINTING THE VALUE OF FLOAT
+
+c = hex(a) #THIS IS A hexadecimal VALUE
+print(c)# PRINTING THE VALUE OF HEXADECIMAL
+
+d = oct(a) #THIS IS A octal VALUE
+print(d)# PRINTING THE VALUE OF OCTAL
+
+e = bin(a) #THIS IS A binary VALUE
+print(e)# PRINTING THE VALUE OF BINARY
+nam = "Y"
+f = ord(nam) #THIS IS A Unicode VALUE
+print(f)# PRINTING THE VALUE OF UNICODE
+
+nam = chr(f) #THIS IS A  Unicode VALUE
+print(nam)# PRINTING THE VALUE OF UNICODE
